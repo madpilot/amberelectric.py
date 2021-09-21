433105b8d0f85f7a244dd2b0d1573cab8dc8d8ee
Myles Eftos <myles@madpilot.com.au>
2021-09-21 21:54:29 +1000

    Fixing type hinting. Fixes #1 and #2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "amberelectric"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 # To install the library, run the following
 #
 # python setup.py install
