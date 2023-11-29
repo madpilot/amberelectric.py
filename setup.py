b034b545858d22bf7a69d94982a99907e23d6d4b
Myles Eftos <myles@madpilot.com.au>
2023-11-29 12:11:41 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "amberelectric"
-VERSION = "1.0.4"
+VERSION = "1.1.0"
 # To install the library, run the following
 #
 # python setup.py install
@@ -24,7 +24,7 @@ setup(
     author_email="dev+pypi@amber.com.au",
     url="https://github.com/madpilot/amberelectric.py",
     keywords=["amberelectric"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
