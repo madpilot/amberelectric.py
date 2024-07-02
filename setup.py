c2c8ff57f9db12c5c989d986e96f27f9d93b1abd
Myles Eftos <myles@madpilot.com.au>
2024-07-02 20:37:32 +1000

    Bump version ready for a new release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "amberelectric"
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 # To install the library, run the following
 #
 # python setup.py install
